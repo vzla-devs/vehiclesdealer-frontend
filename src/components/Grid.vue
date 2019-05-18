a083a6b47a37bb7f9f6b44e666c0b80aafb38be1
Gabriel Nuñez <gabriel.nzn@gmail.com>
2019-05-18 10:45:28 +0100

    extrating variables

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -18,13 +18,13 @@ export default {
   margin: $small-gap;
 }
 
-@media (min-width: 767px) {
+@media (min-width: $small-breakpoint) {
   .grid {
     grid-template-columns: repeat(2, 1fr);
   }
 }
 
-@media (min-width: 1264px) {
+@media (min-width: $medium-breakpoint) {
   .grid {
     grid-template-columns: repeat(3, 1fr);
     grid-gap: $medium-gap;
@@ -32,7 +32,7 @@ export default {
   }
 }
 
-@media (min-width: 1904px) {
+@media (min-width: $large-breakpoint) {
   .grid {
     justify-content: center;
     grid-template-columns: repeat(4, minmax(400px, 540px));
