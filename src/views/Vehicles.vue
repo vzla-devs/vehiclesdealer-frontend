d02de13e4d857e3c8bee188392e34579f8911873
Gabriel Nuñez <gabriel.nzn@gmail.com>
2019-05-18 10:04:59 +0100

    Fixing style

--- src/views/Vehicles.vue
+++ src/views/Vehicles.vue
@@ -25,9 +25,27 @@ export default {
 <style lang="scss" scoped>
 .grid {
   display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  grid-auto-rows: minmax(200px, auto);
+  grid-auto-rows: 400px;
   grid-gap: $gap;
   margin: 0 $gap;
+}
+
+@media (min-width: 767px) {
+  .grid {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+@media (min-width: 1264px) {
+  .grid {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+
+@media (min-width: 1904px) {
+  .grid {
+    justify-content: center;
+    grid-template-columns: repeat(4, minmax(400px, 540px));
+  }
 }
 </style>
