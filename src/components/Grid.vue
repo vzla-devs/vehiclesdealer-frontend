26ebe19ed7e0b521a4e363996ed9205633e0adf2
Gabriel Nuñez <gabriel.nzn@gmail.com>
2019-05-18 10:11:57 +0100

    Using grid component

---
+++ src/components/Grid.vue
@@ -0,0 +1,41 @@
+<template>
+<div class="grid">
+  <slot></slot>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Grid'
+}
+</script>
+
+<style lang="scss" scoped>
+.grid {
+  display: grid;
+  grid-auto-rows: 400px;
+  grid-gap: $small-gap;
+  margin: 0 $small-gap;
+}
+
+@media (min-width: 767px) {
+  .grid {
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+
+@media (min-width: 1264px) {
+  .grid {
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: $medium-gap;
+    margin: 0 $medium-gap;
+  }
+}
+
+@media (min-width: 1904px) {
+  .grid {
+    justify-content: center;
+    grid-template-columns: repeat(4, minmax(400px, 540px));
+  }
+}
+</style>
--- src/views/Vehicles.vue
+++ src/views/Vehicles.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="vehicles">
     <h1>Vehículos</h1>
-    <div class="grid">
+    <grid>
       <card/>
       <card/>
       <card/>
@@ -10,42 +10,24 @@
       <card/>
       <card/>
       <card/>
-    </div>
+      <card/>
+      <card/>
+      <card/>
+      <card/>
+      <card/>
+      <card/>
+      <card/>
+    </grid>
   </div>
 </template>
 <script>
+import Grid from '@/components/Grid'
 import Card from '@/components/Card'
 export default {
   name: 'Vehicles',
   components: {
+    Grid,
     Card
   }
 }
 </script>
-<style lang="scss" scoped>
-.grid {
-  display: grid;
-  grid-auto-rows: 400px;
-  grid-gap: $gap;
-  margin: 0 $gap;
-}
-
-@media (min-width: 767px) {
-  .grid {
-    grid-template-columns: repeat(2, 1fr);
-  }
-}
-
-@media (min-width: 1264px) {
-  .grid {
-    grid-template-columns: repeat(3, 1fr);
-  }
-}
-
-@media (min-width: 1904px) {
-  .grid {
-    justify-content: center;
-    grid-template-columns: repeat(4, minmax(400px, 540px));
-  }
-}
-</style>
