d874056a73e39b2569537f78ab3176698cc42bd1
Gabriel Nuñez <gabriel.nzn@gmail.com>
2019-05-22 06:58:52 +0100

    Rename

---
+++ src/components/VehicleCard.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="card"></div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+  border-radius: $border-radius;
+  background-color: lightgray;
+}
+</style>
--- src/views/Vehicles.vue
+++ src/views/Vehicles.vue
@@ -2,18 +2,18 @@
   <div>
     <h1>Vehículos</h1>
     <grid>
-      <card v-for="(vehicle, index) in vehicles" :key="index" class="vehicle" />
+      <vehicle-card v-for="(vehicle, index) in vehicles" :key="index" class="vehicle" />
     </grid>
   </div>
 </template>
 <script>
 import { mapState } from 'vuex'
 import Grid from '@/components/Grid'
-import Card from '@/components/Card'
+import VehicleCard from '@/components/VehicleCard'
 export default {
   components: {
     Grid,
-    Card
+    VehicleCard
   },
   computed: {
     ...mapState({ vehicles: state => state.vehicles })
