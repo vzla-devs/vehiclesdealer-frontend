eb832203757356b44de5fd66032328095ee89fa4
gnunez <gnunez@aidacanarias.com>
2019-06-20 18:29:51 +0100

    refactor: extracting method

--- vehiclesdealer-vue/tests/unit/store/getters/vehiclesGetters.spec.js
+++ vehiclesdealer-vue/tests/unit/store/getters/vehiclesGetters.spec.js
@@ -4,34 +4,20 @@ import initialState from '@/store/initialState'
 
 describe('vehiclesGetters.js', () => {
   test('get vehicles', () => {
-    const vehicles = [
-      {
-        brand: 'firstBrand',
-        model: 'firstModel',
-        year: 2017,
-        price: 7777,
-        imageUrl: 'firstUrl'
-      },
-      {
-        brand: 'secondBrand',
-        model: 'secondModel',
-        year: 2018,
-        price: 8888,
-        imageUrl: 'secondUrl'
-      },
-      {
-        brand: 'thirdBrand',
-        model: 'thirdModel',
-        year: 2019,
-        price: 9999,
-        imageUrl: 'thirdUrl'
-      }
+    const givenVehicles = [
+      givenAVehicle({ brand: 'firstBrand', model: 'firstModel', year: 2019, price: 9999, imageUrl: 'firstUrl' }),
+      givenAVehicle({ brand: 'secondBrand', model: 'secondModel', year:  2019, price: 9999, imageUrl: 'secondUrl' }),
+      givenAVehicle({ brand: 'thirdBrand', model: 'thirdModel', year: 2019, price: 9999, imageUrl: 'thirdUrl' })
     ]
-    const state = Object.assign({}, initialState, { vehicles })
+    const state = Object.assign({}, initialState, { vehicles: givenVehicles })
   
     const vehiclesFromGetter = vehiclesGetters[GET_VEHICLES](state)
   
-    expect(vehiclesFromGetter).toEqual(vehicles)
-    expect(vehiclesFromGetter).not.toBe(vehicles)
+    expect(vehiclesFromGetter).toEqual(givenVehicles)
+    expect(vehiclesFromGetter).not.toBe(givenVehicles)
   })
+
+  function givenAVehicle ({ brand, model, year, price, imageUrl } = {}) {
+    return { brand, model, year, price, imageUrl }
+  }
 })
