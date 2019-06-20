6346bf323051aa333cafbb6d488f334c5a6fdb6b
gnunez <gnunez@aidacanarias.com>
2019-06-20 19:26:58 +0100

    rename

--- vehicledealers-dotnetcore/VehiclesDealerApi/Controllers/VehiclesController.cs
+++ vehicledealers-dotnetcore/VehiclesDealerApi/Controllers/VehiclesController.cs
@@ -8,35 +8,35 @@ namespace VehiclesDealerApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class VehiclesController : ControllerBase
     {
-        // GET api/values
+        // GET api/vehicles
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
+        // GET api/vehicles/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
             return "value";
         }
 
-        // POST api/values
+        // POST api/vehicles
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT api/vehicles/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE api/vehicles/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
