fb9f5b8c0d0d2cb0c8f0d25838a0bbeac12605e6
gnunez <gnunez@aidacanarias.com>
2019-06-21 21:37:09 +0100

    rename

--- vehiclesdealer-vue/tests/unit/store/getters/vehiclesGetters.spec.js
+++ vehiclesdealer-vue/tests/unit/store/getters/vehiclesGetters.spec.js
@@ -5,9 +5,9 @@ import initialState from '@/store/initialState'
 describe('vehiclesGetters.js', () => {
   test('get vehicles', () => {
     const givenVehicles = [
-      givenAVehicle({ brand: 'firstBrand', model: 'firstModel', year: 2019, price: 9999, imageUrl: 'firstUrl' }),
-      givenAVehicle({ brand: 'secondBrand', model: 'secondModel', year:  2019, price: 9999, imageUrl: 'secondUrl' }),
-      givenAVehicle({ brand: 'thirdBrand', model: 'thirdModel', year: 2019, price: 9999, imageUrl: 'thirdUrl' })
+      givenAVehicleFromState({ brand: 'firstBrand', model: 'firstModel', year: 2019, price: 9999, imageUrl: 'firstUrl' }),
+      givenAVehicleFromState({ brand: 'secondBrand', model: 'secondModel', year:  2019, price: 9999, imageUrl: 'secondUrl' }),
+      givenAVehicleFromState({ brand: 'thirdBrand', model: 'thirdModel', year: 2019, price: 9999, imageUrl: 'thirdUrl' })
     ]
     const givenState = Object.assign({}, initialState, { vehicles: givenVehicles })
   
@@ -17,7 +17,7 @@ describe('vehiclesGetters.js', () => {
     expect(vehiclesFromGetter).not.toBe(givenVehicles)
   })
 
-  function givenAVehicle ({ brand, model, year, price, imageUrl }) {
+  function givenAVehicleFromState ({ brand, model, year, price, imageUrl }) {
     return { brand, model, year, price, imageUrl }
   }
 })
