ae13b7325df2302f5cd9b7989a8551fb3f076920
gnunez <gnunez@aidacanarias.com>
2019-06-22 19:34:14 +0100

    Refactor

--- vehiclesdealer-vue/tests/unit/store/actions/vehiclesActions.spec.js
+++ vehiclesdealer-vue/tests/unit/store/actions/vehiclesActions.spec.js
@@ -3,38 +3,40 @@ import { GET_VEHICLES } from '@/store/actions/actionsTypes'
 import { SET_VEHICLES } from '@/store/mutations/mutationsTypes'
 import { GET_VEHICLES_URL } from '@/constants/serverRoutes'
 import axios from 'axios'
-
 jest.mock('axios')
 
 describe('vehiclesActions.js', () => {
+  let mockedContext
+  beforeEach(() => {
+    mockedContext = { 'commit': jest.fn() }
+  })
+
   test('get vehicles from the API and then commit the corresponding mutation', async () => {
-    const mockCommit = jest.fn()
-    const context = { 'commit': mockCommit }
     const vehicles = [
       givenAVehicleFromAPI({ brand: 'anyBrand', model: 'anyModel', year: 2019, price: 9999, imageUrl: 'anyUrl' })
     ]
-    axios.get.mockImplementation(() => Promise.resolve({ data: vehicles }))
+    const resolvedPromise = Promise.resolve({ data: vehicles })
+    axios.get.mockImplementation(() => resolvedPromise)
 
-    const promise = vehiclesActions[GET_VEHICLES](context)
+    const promise = vehiclesActions[GET_VEHICLES](mockedContext)
 
     expect(axios.get).toHaveBeenCalledWith(GET_VEHICLES_URL)
-    expect(context.commit).not.toHaveBeenCalled()
+    expect(mockedContext.commit).not.toHaveBeenCalled()
     await promise
-    expect(context.commit).toHaveBeenCalledWith(SET_VEHICLES, vehicles)
+    expect(mockedContext.commit).toHaveBeenCalledWith(SET_VEHICLES, vehicles)
   })
 
   test('do not commit the corresponding mutation when it gets a failure from the API', async () => {
-    const mockCommit = jest.fn()
-    const context = { 'commit': mockCommit }
     const reason = 'error'
-    axios.get.mockImplementation(() => Promise.reject(reason))
+    const rejectedPromise = Promise.reject(reason)
+    axios.get.mockImplementation(() => rejectedPromise)
 
-    const promise = vehiclesActions[GET_VEHICLES](context)
+    const promise = vehiclesActions[GET_VEHICLES](mockedContext)
 
     expect(axios.get).toHaveBeenCalledWith(GET_VEHICLES_URL)
-    expect(context.commit).not.toHaveBeenCalled()
+    expect(mockedContext.commit).not.toHaveBeenCalled()
     const expectedResponse = await promise
-    expect(context.commit).not.toHaveBeenCalled()
+    expect(mockedContext.commit).not.toHaveBeenCalled()
     expect(expectedResponse).toBe(reason)
   })
 
