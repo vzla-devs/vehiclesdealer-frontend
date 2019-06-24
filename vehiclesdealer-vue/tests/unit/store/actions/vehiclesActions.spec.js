77c64f221480fc4f130174415d7c9b009a6eb461
gnunez <gnunez@aidacanarias.com>
2019-06-24 11:38:25 +0100

    Renaming tests

--- vehiclesdealer-vue/tests/unit/store/actions/vehiclesActions.spec.js
+++ vehiclesdealer-vue/tests/unit/store/actions/vehiclesActions.spec.js
@@ -12,33 +12,35 @@ describe('vehiclesActions.js', () => {
     mockedContext = { 'commit': jest.fn() }
   })
 
-  test('get vehicles from the API and then commit the corresponding mutation', async () => {
-    const vehicles = [
-      givenAVehicleFromAPI({ brand: 'anyBrand', model: 'anyModel', year: 2019, price: 9999, imageUrl: 'anyUrl' })
-    ]
-    const resolvedPromise = Promise.resolve({ data: vehicles })
-    mockGetImplementation(resolvedPromise)
-
-    const promise = vehiclesActions[GET_VEHICLES](mockedContext)
-
-    expect(axios.get).toHaveBeenCalledWith(GET_VEHICLES_URL)
-    expect(mockedContext.commit).not.toHaveBeenCalled()
-    await promise
-    expect(mockedContext.commit).toHaveBeenCalledWith(SET_VEHICLES, vehicles)
-  })
-
-  test('do not commit the corresponding mutation when it gets a failure from the API', async () => {
-    const reason = 'error'
-    const rejectedPromise = Promise.reject(reason)
-    mockGetImplementation(rejectedPromise)
-
-    const promise = vehiclesActions[GET_VEHICLES](mockedContext)
-
-    expect(axios.get).toHaveBeenCalledWith(GET_VEHICLES_URL)
-    expect(mockedContext.commit).not.toHaveBeenCalled()
-    const expectedResponse = await promise
-    expect(mockedContext.commit).not.toHaveBeenCalled()
-    expect(expectedResponse).toBe(reason)
+  describe('when getting the vehicles from the API', () => {
+    test('commit the corresponding mutation after a successful response', async () => {
+      const vehicles = [
+        givenAVehicleFromAPI({ brand: 'anyBrand', model: 'anyModel', year: 2019, price: 9999, imageUrl: 'anyUrl' })
+      ]
+      const resolvedPromise = Promise.resolve({ data: vehicles })
+      mockGetImplementation(resolvedPromise)
+  
+      const promise = vehiclesActions[GET_VEHICLES](mockedContext)
+  
+      expect(axios.get).toHaveBeenCalledWith(GET_VEHICLES_URL)
+      expect(mockedContext.commit).not.toHaveBeenCalled()
+      await promise
+      expect(mockedContext.commit).toHaveBeenCalledWith(SET_VEHICLES, vehicles)
+    })
+  
+    test('do not commit the corresponding mutation after a failed response and return the error', async () => {
+      const reason = 'error'
+      const rejectedPromise = Promise.reject(reason)
+      mockGetImplementation(rejectedPromise)
+  
+      const promise = vehiclesActions[GET_VEHICLES](mockedContext)
+  
+      expect(axios.get).toHaveBeenCalledWith(GET_VEHICLES_URL)
+      expect(mockedContext.commit).not.toHaveBeenCalled()
+      const expectedResponse = await promise
+      expect(mockedContext.commit).not.toHaveBeenCalled()
+      expect(expectedResponse).toBe(reason)
+    })
   })
 
   function givenAVehicleFromAPI ({ brand, model, year, price, imageUrl }) {
