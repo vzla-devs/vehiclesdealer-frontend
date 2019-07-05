83734bbbb23aa080427a6c77d8a74adec112f031
gnunez <gnunez@aidacanarias.com>
2019-07-05 22:28:17 +0100

    Refactor

--- vehiclesdealer-vue/tests/unit/components/basic/ErrorBanner.spec.js
+++ vehiclesdealer-vue/tests/unit/components/basic/ErrorBanner.spec.js
@@ -2,44 +2,23 @@ import ErrorBanner from '@/components/basic/ErrorBanner'
 import { wrapperBuilderFactory } from '@/helpers/factoryHelpers'
 
 describe( 'ErrorBanner.vue', () => {
-
   it ('should render correctly', () => {
     const message = 'anyMessage'
 
-    const wrapper = errorBannerBuilder().withMessage(message).build()
+    const wrapper = errorBannerBuilder().withProps({ message }).build()
 
-    expect(wrapper.snapshot()).toMatchSnapshot()
+    expect(wrapper.element).toMatchSnapshot()
   })
 
   it ('should emit onClose event when closing the banner', () => {
     const wrapper = errorBannerBuilder().build()
 
-    wrapper.closeErrorBanner()
+    wrapper.find('.error-banner').vm.$emit('input')
 
     expect(wrapper.emitted().onClose).toBeTruthy()
   })
 
   function errorBannerBuilder () {
-    let message = ''
-    let wrapper
-
-    function withMessage (newMessage) {
-      message = newMessage
-      return self
-    }
-
-    function build () {
-      wrapper = wrapperBuilderFactory({ component: ErrorBanner, stubs: ['v-alert'] }).withProps({ message }).build()
-      return self
-    }
-
-    const self = {
-      withMessage,
-      build,
-      snapshot: () => wrapper.element,
-      closeErrorBanner: () => wrapper.find('.error-banner').vm.$emit('input'),
-      emitted: () => wrapper.emitted()
-    }
-    return self
+    return wrapperBuilderFactory({ component: ErrorBanner, stubs: ['v-alert'] })
   }
 })
