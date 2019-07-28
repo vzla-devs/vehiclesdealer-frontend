377b42a3b19115bed15a08d7bbd958faa48654b3
gnunez <gnunez@aidacanarias.com>
2019-07-28 19:54:01 +0100

    should commit the corresponding mutation to clear an error message

--- vehiclesdealer-vue/src/store/actions/messagesActions.js
+++ vehiclesdealer-vue/src/store/actions/messagesActions.js
@@ -1,8 +1,11 @@
-import { SHOW_MESSAGE } from '@/store/actions/actionsTypes'
-import { SET_MESSAGE } from '@/store/mutations/mutationsTypes'
+import { SHOW_MESSAGE, CLEAR_MESSAGE } from '@/store/actions/actionsTypes'
+import { SET_MESSAGE, RESET_MESSAGE } from '@/store/mutations/mutationsTypes'
 
 export default {
   [SHOW_MESSAGE]: ({ commit }, { type, message }) => {
     commit(SET_MESSAGE, { type, message })
+  },
+  [CLEAR_MESSAGE]: ({ commit }, type) => {
+    commit(RESET_MESSAGE, type)
   }
 }
--- vehiclesdealer-vue/tests/unit/store/actions/messagesActions.spec.js
+++ vehiclesdealer-vue/tests/unit/store/actions/messagesActions.spec.js
@@ -1,6 +1,6 @@
 import messagesActions from '@/store/actions/messagesActions'
-import { SHOW_MESSAGE } from '@/store/actions/actionsTypes'
-import { SET_MESSAGE } from '@/store/mutations/mutationsTypes'
+import { SHOW_MESSAGE, CLEAR_MESSAGE } from '@/store/actions/actionsTypes'
+import { SET_MESSAGE, RESET_MESSAGE } from '@/store/mutations/mutationsTypes'
 import messagesTypes from '@/enums/messagesTypes'
 
 describe('messagesActions.js', () => {
@@ -19,5 +19,13 @@ describe('messagesActions.js', () => {
     messagesActions[SHOW_MESSAGE](mockedContext, givenPayload)
 
     expect(mockedContext.commit).toHaveBeenCalledWith(SET_MESSAGE, givenPayload)
+  })
+
+  test('should commit the corresponding mutation to clear an error message', () => {
+    const givenType = messagesTypes().error
+
+    messagesActions[CLEAR_MESSAGE](mockedContext, givenType)
+
+    expect(mockedContext.commit).toHaveBeenCalledWith(RESET_MESSAGE, givenType)
   })
 })
