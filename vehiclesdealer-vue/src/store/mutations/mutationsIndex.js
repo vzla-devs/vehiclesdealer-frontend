8074cf5127be1b9f142c3d1e9d6decf5e53b7364
gnunez <gnunez@aidacanarias.com>
2019-08-27 20:13:20 +0100

    adding new mutations to the index

--- vehiclesdealer-vue/src/store/mutations/mutationsIndex.js
+++ vehiclesdealer-vue/src/store/mutations/mutationsIndex.js
@@ -1,7 +1,9 @@
 import vehiclesMutations from './vehiclesMutations'
 import messagesMutations from './messagesMutations'
+import applicationMutations from './applicationMutations'
 
 export default {
   ...vehiclesMutations,
-  ...messagesMutations
+  ...messagesMutations,
+  ...applicationMutations
 }
