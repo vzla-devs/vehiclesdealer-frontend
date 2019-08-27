624016a0df3465f94de493363e4a2e112ccb874c
gnunez <gnunez@aidacanarias.com>
2019-08-27 20:06:02 +0100

    updated test

--- vehiclesdealer-vue/src/store/actions/vehiclesActions.js
+++ vehiclesdealer-vue/src/store/actions/vehiclesActions.js
@@ -1,10 +1,11 @@
 import { GET_VEHICLES, SHOW_MESSAGE } from '@/store/actions/actionTypes'
-import { SET_VEHICLES } from '@/store/mutations/mutationTypes'
+import { SET_VEHICLES, SET_LOADING, RESET_LOADING } from '@/store/mutations/mutationTypes'
 import { VehiclesClient } from '@/clients/clientsFactory'
 import { MESSAGE_TYPES } from '@/constants/enums'
 
 export default {
   [GET_VEHICLES]: async ({ commit, dispatch }) => {
+    commit(SET_LOADING)
     try {
       const response = await VehiclesClient.get()
       commit(SET_VEHICLES, response.data)
@@ -12,5 +13,6 @@ export default {
       dispatch(SHOW_MESSAGE, { type: MESSAGE_TYPES.ERROR, message: 'Ha ocurrido un error' })
       return error
     }
+    commit(RESET_LOADING)
   }
 }
--- vehiclesdealer-vue/tests/unit/store/actions/vehiclesActions.spec.js
+++ vehiclesdealer-vue/tests/unit/store/actions/vehiclesActions.spec.js
@@ -1,6 +1,6 @@
 import Actions from '@/store/actions/vehiclesActions'
 import { GET_VEHICLES, SHOW_MESSAGE } from '@/store/actions/actionTypes'
-import { SET_VEHICLES } from '@/store/mutations/mutationTypes'
+import { SET_VEHICLES, SET_LOADING, RESET_LOADING } from '@/store/mutations/mutationTypes'
 import { VehiclesClient } from '@/clients/clientsFactory'
 import { resolvedPromise, rejectedPromise } from '@tests/helpers/testHelpers'
 import testValues from '@tests/helpers/testValues'
@@ -13,9 +13,12 @@ describe('vehiclesActions.js', () => {
       const vehicles = [ testValues.vehicle({ id: '1' }), testValues.vehicle({ id: '2' }) ]
       VehiclesClient.get = jest.fn(() => resolvedPromise({ data: vehicles }))
 
-      await Actions[GET_VEHICLES]({ commit })
+      const returnedPromise = Actions[GET_VEHICLES]({ commit })
 
+      expect(commit).toHaveBeenCalledWith(SET_LOADING)
       expect(VehiclesClient.get).toHaveBeenCalled()
+      await returnedPromise
+      expect(commit).toHaveBeenCalledWith(RESET_LOADING)
       expect(commit).toHaveBeenCalledWith(SET_VEHICLES, vehicles)
     })
 
