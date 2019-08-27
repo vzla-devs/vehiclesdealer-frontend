49119b8e3ac2ca37017a58ebda5fa1adf8c28ee2
gnunez <gnunez@aidacanarias.com>
2019-08-27 19:57:28 +0100

    green: should set the loading state to false

--- vehiclesdealer-vue/tests/unit/store/mutations/applicationMutations.spec.js
+++ vehiclesdealer-vue/tests/unit/store/mutations/applicationMutations.spec.js
@@ -1,14 +1,23 @@
 import Mutations from '@/store/mutations/applicationMutations'
-import { SET_LOADING } from '@/store/mutations/mutationTypes'
+import { SET_LOADING, RESET_LOADING } from '@/store/mutations/mutationTypes'
 import { buildStateWith } from '@tests/helpers/builderHelpers'
 
 describe('applicationMutations.js', () => {
-  test('should mutate the state with the loading state', () => {
+  test('should set the loading state to true', () => {
     const givenState = buildStateWith({ loading: false })
 
     Mutations[SET_LOADING](givenState)
 
     const expectedState = buildStateWith({ loading: true })
+    expect(givenState).toEqual(expectedState)
+  })
+
+  test('should set the loading state to false', () => {
+    const givenState = buildStateWith({ loading: true })
+
+    Mutations[RESET_LOADING](givenState)
+
+    const expectedState = buildStateWith({ loading: false })
     expect(givenState).toEqual(expectedState)
   })
 })
