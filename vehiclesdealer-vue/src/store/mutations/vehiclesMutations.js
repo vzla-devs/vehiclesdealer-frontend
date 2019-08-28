71b516c7b5cff2b21fed008d7c14eb8132101bc2
gnunez <gnunez@aidacanarias.com>
2019-08-28 20:08:26 +0100

    rename

--- vehiclesdealer-vue/src/store/mutations/vehiclesMutations.js
+++ vehiclesdealer-vue/src/store/mutations/vehiclesMutations.js
@@ -1,16 +1,16 @@
-import { GET_VEHICLES, GET_VEHICLES_SUCCESS, GET_VEHICLES_FAILURE } from '@/store/mutations/mutationTypes'
+import { SET_VEHICLES, SET_VEHICLES_SUCCESS, SET_VEHICLES_FAILURE } from '@/store/mutations/mutationTypes'
 
 export default {
-  [GET_VEHICLES]: (state) => {
+  [SET_VEHICLES]: (state) => {
     state.vehiclesState.loading = true
     state.vehiclesState.vehicles = []
     state.vehiclesState.error = { show: false, message: '' }
   },
-  [GET_VEHICLES_SUCCESS]: (state, vehicles) => {
+  [SET_VEHICLES_SUCCESS]: (state, vehicles) => {
     state.vehiclesState.vehicles = vehicles
     state.vehiclesState.loading = false
   },
-  [GET_VEHICLES_FAILURE]: (state, message) => {
+  [SET_VEHICLES_FAILURE]: (state, message) => {
     state.vehiclesState.error.show = true
     state.vehiclesState.error.message = message
     state.vehiclesState.loading = false
--- vehiclesdealer-vue/tests/unit/store/mutations/vehiclesMutations.spec.js
+++ vehiclesdealer-vue/tests/unit/store/mutations/vehiclesMutations.spec.js
@@ -1,5 +1,5 @@
 import Mutations from '@/store/mutations/vehiclesMutations'
-import { GET_VEHICLES, GET_VEHICLES_SUCCESS, GET_VEHICLES_FAILURE } from '@/store/mutations/mutationTypes'
+import { SET_VEHICLES, SET_VEHICLES_SUCCESS, SET_VEHICLES_FAILURE } from '@/store/mutations/mutationTypes'
 import { buildStateWith } from '@tests/helpers/builderHelpers'
 import testValues from '@tests/helpers/testValues'
 
@@ -7,7 +7,7 @@ describe('vehiclesMutations.js', () => {
   it('reset the vehicles state and set the loading state to true', () => {
     const givenState = buildStateWith({ vehiclesState: { vehicles: [{ id: 1 }], loading: false, error: { show: true, message: 'anyMessage' } } })
 
-    Mutations[GET_VEHICLES](givenState)
+    Mutations[SET_VEHICLES](givenState)
 
     const expectedState = buildStateWith({ vehiclesState: { vehicles: [], loading: true, error: { show: false, message: '' } } })
     expect(givenState).toEqual(expectedState)
@@ -20,7 +20,7 @@ describe('vehiclesMutations.js', () => {
       testValues.vehicle({ brand: 'secondBrand', model: 'secondModel', year: 2019, price: 9999, imageUrl: 'secondUrl' })
     ]
 
-    Mutations[GET_VEHICLES_SUCCESS](givenState, vehicles)
+    Mutations[SET_VEHICLES_SUCCESS](givenState, vehicles)
 
     const expectedState = buildStateWith({ vehiclesState: { vehicles, loading: false } })
     expect(givenState).toEqual(expectedState)
@@ -30,7 +30,7 @@ describe('vehiclesMutations.js', () => {
     const errorMessage = 'anyErrorMessage'
     const givenState = buildStateWith({ vehiclesState: { loading: true, error: { show: false, message: '' } } })
 
-    Mutations[GET_VEHICLES_FAILURE](givenState, errorMessage)
+    Mutations[SET_VEHICLES_FAILURE](givenState, errorMessage)
 
     const expectedState = buildStateWith({ vehiclesState: { loading: false, error: { show: true, message: errorMessage } } })
     expect(givenState).toEqual(expectedState)
