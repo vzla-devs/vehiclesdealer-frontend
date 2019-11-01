2d6977be9104f730423c73feeb742f03c35f0ff0
gnunez <gnunez@aidacanarias.com>
2019-11-01 11:50:28 +0000

    creating frontend project with the new vue cli

---
+++ frontend/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+      </v-toolbar-title>
+    </v-app-bar>
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="home">
+    <h1>This is a homepage</h1>
+  </div>
+</template>
