81c410a267a99b7019c4f2889d2ad81696dfc07a
gnunez <gnunez@aidacanarias.com>
2019-12-20 08:45:22 +0000

    typying getters funtions

--- vehiclesdealer-frontend/src/store/getters/applicationGetters.ts
+++ vehiclesdealer-frontend/src/store/getters/applicationGetters.ts
@@ -2,10 +2,10 @@ import { Getter } from '@/store/getters/types'
 import { RootState } from '@/store/interfaces/rootState'
 
 export default {
-  [Getter.IS_LOADING]: (state: RootState) => {
+  [Getter.IS_LOADING]: function (state: RootState): boolean {
     return state.loading
   },
-  [Getter.ERROR_MESSAGE]: (state: RootState) => {
+  [Getter.ERROR_MESSAGE]: function (state: RootState): string {
     if (state.messages.error.length > 0) {
       return state.messages.error[0].message
     }
--- vehiclesdealer-frontend/src/store/getters/vehiclesGetters.ts
+++ vehiclesdealer-frontend/src/store/getters/vehiclesGetters.ts
@@ -2,7 +2,7 @@ import { RootState } from '@/store/interfaces/rootState'
 import { Getter } from '@/store/getters/types'
 
 export default {
-  [Getter.AVAILABLE_VEHICLES]: (state: RootState) => {
+  [Getter.AVAILABLE_VEHICLES]: function (state: RootState): any {
     return state.vehicles
   }
 }
