0b0d3326b72d44f8e6465f75b7e4f0cd4d70d5dc
gnunez <gnunez@aidacanarias.com>
2019-12-20 08:56:39 +0000

    green: adding and removing notification messages

--- vehiclesdealer-frontend/tests/unit/store/actions/applicationActions.spec.ts
+++ vehiclesdealer-frontend/tests/unit/store/actions/applicationActions.spec.ts
@@ -2,6 +2,7 @@ import actions from '@/store/actions/applicationActions'
 import { ErrorMessage } from '@/store/models/errorMessage'
 import { Mutation } from '@/store/mutations/types'
 import { Message } from '@/store/types/message'
+import { NotificationMessage } from '@/store/models/notificationMessage'
 
 describe('applicationActions.js', () => {
   it('commits the corresponding mutation to show an error message', () => {
@@ -19,5 +20,22 @@ describe('applicationActions.js', () => {
     actions.CLEAR_MESSAGE ({ commit }, Message.Error)
 
     expect(commit).toHaveBeenCalledWith(Mutation.REMOVE_APPLICATION_MESSAGE, Message.Error)
+  })
+
+  it('commits the corresponding mutation to show a notification message', () => {
+    const commit = jest.fn()
+    const message = new NotificationMessage('anyMessage')
+
+    actions.SHOW_MESSAGE({ commit }, message)
+
+    expect(commit).toHaveBeenCalledWith(Mutation.ADD_APPLICATION_MESSAGE, message)
+  })
+
+  it('commits the corresponding mutation to clear a notification message', () => {
+    const commit = jest.fn()
+
+    actions.CLEAR_MESSAGE ({ commit }, Message.Notification)
+
+    expect(commit).toHaveBeenCalledWith(Mutation.REMOVE_APPLICATION_MESSAGE, Message.Notification)
   })
 })
