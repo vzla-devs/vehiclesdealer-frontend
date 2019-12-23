40d23ae0b07f947e1def6179e3d2a4c585da74c3
gnunez <gnunez@aidacanarias.com>
2019-12-23 15:57:50 +0000

    ignore

--- vehiclesdealer-frontend/src/store/index.ts
+++ vehiclesdealer-frontend/src/store/index.ts
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import { InitialState } from '@/store/models/initialState'
+import { InitialState } from './models/initialState'
+import { RootState } from './models/rootState'
 import mutations from './mutations'
 import actions from './actions'
 import getters from './getters'
 Vue.use(Vuex)
 
-export default new Vuex.Store({
+export default new Vuex.Store<RootState>({
   state: new InitialState(),
   mutations,
   actions,
