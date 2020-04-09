b84a10e0b0edc712c16d3deffc2dfe6742483316
Gabriel Nunez <gnunez@aidacanarias.com>
2020-04-09 19:01:10 +0100

    rename

--- vehiclesdealer-frontend/tests/unit/store/actions/vehiclesActions.spec.ts
+++ vehiclesdealer-frontend/tests/unit/store/actions/vehiclesActions.spec.ts
@@ -8,32 +8,32 @@ import { ApplicationMessage } from '@/store/interfaces/applicationMessage'
 
 describe('vehiclesActions.js', () => {
   it('calls the client to get the vehicles, stores them and returns them', async () => {
-    const context = { commit: jest.fn(), dispatch: jest.fn() }
-    const vehicles = [ TestValues.vehicle('1'), TestValues.vehicle('2') ]
-    VehiclesClient.get = jest.fn(() => resolvedPromise({ data: vehicles }))
+    const givenContext = { commit: jest.fn(), dispatch: jest.fn() }
+    const givenVehicles = [ TestValues.vehicle('1'), TestValues.vehicle('2') ]
+    VehiclesClient.get = jest.fn(() => resolvedPromise({ data: givenVehicles }))
 
-    const returnedPromise = actions[Action.GET_VEHICLES](context)
+    const returnedPromise = actions[Action.GET_VEHICLES](givenContext)
 
     expect(VehiclesClient.get).toHaveBeenCalled()
-    expect(context.commit).toHaveBeenCalledWith(Mutation.SET_APPLICATION_LOADING, true)
+    expect(givenContext.commit).toHaveBeenCalledWith(Mutation.SET_APPLICATION_LOADING, true)
     await returnedPromise
-    expect(context.commit).toHaveBeenCalledWith(Mutation.SET_VEHICLES, vehicles)
-    expect(context.commit).toHaveBeenCalledWith(Mutation.SET_APPLICATION_LOADING, false)
-    expect(context.dispatch).not.toHaveBeenCalledWith(Action.SHOW_MESSAGE)
+    expect(givenContext.commit).toHaveBeenCalledWith(Mutation.SET_VEHICLES, givenVehicles)
+    expect(givenContext.commit).toHaveBeenCalledWith(Mutation.SET_APPLICATION_LOADING, false)
+    expect(givenContext.dispatch).not.toHaveBeenCalledWith(Action.SHOW_MESSAGE)
   })
 
   it('shows an error message after getting the vehicles fails', async () => {
-    const context = { commit: jest.fn(), dispatch: jest.fn() }
+    const givenContext = { commit: jest.fn(), dispatch: jest.fn() }
     VehiclesClient.get = jest.fn(() => rejectedPromise())
 
-    const returnedPromise = actions[Action.GET_VEHICLES](context)
+    const returnedPromise = actions[Action.GET_VEHICLES](givenContext)
 
     expect(VehiclesClient.get).toHaveBeenCalled()
-    expect(context.commit).toHaveBeenCalledWith(Mutation.SET_APPLICATION_LOADING, true)
+    expect(givenContext.commit).toHaveBeenCalledWith(Mutation.SET_APPLICATION_LOADING, true)
     await returnedPromise
-    expect(context.commit).toHaveBeenCalledWith(Mutation.SET_APPLICATION_LOADING, false)
-    expect(context.commit).not.toHaveBeenCalledWith(Mutation.SET_VEHICLES)
+    expect(givenContext.commit).toHaveBeenCalledWith(Mutation.SET_APPLICATION_LOADING, false)
+    expect(givenContext.commit).not.toHaveBeenCalledWith(Mutation.SET_VEHICLES)
     const expectedErrorMessage: ApplicationMessage = { type: 'error', message: 'ha ocurrido un error' }
-    expect(context.dispatch).toHaveBeenCalledWith(Action.SHOW_MESSAGE, expectedErrorMessage)
+    expect(givenContext.dispatch).toHaveBeenCalledWith(Action.SHOW_MESSAGE, expectedErrorMessage)
   })
 })
