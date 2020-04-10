8ce3591f0f27c1e8d46af52b4a9736e90bfd2805
Gabriel Nunez <gnunez@aidacanarias.com>
2020-04-10 13:03:15 +0100

    refactor: extracting try catch block

--- vehiclesdealer-frontend/src/store/actions/vehiclesActions.ts
+++ vehiclesdealer-frontend/src/store/actions/vehiclesActions.ts
@@ -1,18 +1,15 @@
 import { VehiclesClient } from '@/clients/clientsFactory'
 import { Action } from './types'
 import { Mutation } from '@/store/mutations/types'
-import { ApplicationMessage } from '@/store/interfaces/applicationMessage'
+import { TryThisAction } from '@/store/actions/actionDecorators'
 
-export default {
-  [Action.GET_VEHICLES]: async ({ commit, dispatch }) => {
-    commit(Mutation.SET_APPLICATION_LOADING, true)
-    try {
-      const response = await VehiclesClient.get()
-      commit(Mutation.SET_VEHICLES, response.data)
-    } catch (error) {
-      const errorMessage: ApplicationMessage = { type: 'error', message: 'ha ocurrido un error' }
-      dispatch(Action.SHOW_MESSAGE, errorMessage)
-    }
-    commit(Mutation.SET_APPLICATION_LOADING, false)
-  }
+async function getVehicles ({ commit, dispatch }) {
+  const response = await VehiclesClient.get()
+  commit(Mutation.SET_VEHICLES, response.data)
 }
+
+const actions = {
+  [Action.GET_VEHICLES]: TryThisAction(getVehicles)
+}
+
+export default actions
--- vehiclesdealer-frontend/tests/unit/store/actions/vehiclesActions.spec.ts
+++ vehiclesdealer-frontend/tests/unit/store/actions/vehiclesActions.spec.ts
@@ -4,42 +4,35 @@ import { resolvedPromise, rejectedPromise } from '@tests/helpers/promiseHelpers'
 import { TestValues } from '@tests/helpers/testValues'
 import { Action } from '@/store/actions/types'
 import { Mutation } from '@/store/mutations/types'
-import { ApplicationMessage } from '@/store/interfaces/applicationMessage'
 
 describe('vehiclesActions.js', () => {
-  it('calls the client to get the vehicles, stores them and returns them', async () => {
+  it('calls the client to get the vehicles and commits the results', async () => {
     const givenContext = getMockedContext()
     const givenVehicles = [ TestValues.vehicle('1'), TestValues.vehicle('2') ]
     mockClientGetWith(resolvedPromise({ data: givenVehicles }))
 
     const returnedPromise = actions[Action.GET_VEHICLES](givenContext)
 
-    expect(givenContext.commit).toHaveBeenCalledWith(Mutation.SET_APPLICATION_LOADING, true)
-    await returnedPromise
     expect(VehiclesClient.get).toHaveBeenCalled()
+    await returnedPromise
     expect(givenContext.commit).toHaveBeenCalledWith(Mutation.SET_VEHICLES, givenVehicles)
-    expect(givenContext.commit).toHaveBeenCalledWith(Mutation.SET_APPLICATION_LOADING, false)
-    expect(givenContext.dispatch).not.toHaveBeenCalledWith(Action.SHOW_MESSAGE)
   })
 
-  it('shows an error message after getting the vehicles fails', async () => {
+  it('does not commit the results when getting the vehicles fails', async () => {
     const givenContext = getMockedContext()
     mockClientGetWith(rejectedPromise())
 
     const returnedPromise = actions[Action.GET_VEHICLES](givenContext)
 
     expect(VehiclesClient.get).toHaveBeenCalled()
-    expect(givenContext.commit).toHaveBeenCalledWith(Mutation.SET_APPLICATION_LOADING, true)
     await returnedPromise
-    expect(givenContext.commit).toHaveBeenCalledWith(Mutation.SET_APPLICATION_LOADING, false)
     expect(givenContext.commit).not.toHaveBeenCalledWith(Mutation.SET_VEHICLES)
-    const expectedErrorMessage: ApplicationMessage = { type: 'error', message: 'ha ocurrido un error' }
-    expect(givenContext.dispatch).toHaveBeenCalledWith(Action.SHOW_MESSAGE, expectedErrorMessage)
   })
 
   function getMockedContext (): any {
     return { commit: jest.fn(), dispatch: jest.fn() }
   }
+  
   function mockClientGetWith (promise: Promise<any>): void {
     VehiclesClient.get = jest.fn(() => promise)
   }
