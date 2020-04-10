cc680e2d225b9c3afe0834031197e7238b92763b
Gabriel Nunez <gnunez@aidacanarias.com>
2020-04-10 12:58:08 +0100

    green: calls the corresponding action and shows an error message when it fails

--- vehiclesdealer-frontend/tests/unit/store/actions/actionDecorators.spec.ts
+++ vehiclesdealer-frontend/tests/unit/store/actions/actionDecorators.spec.ts
@@ -1,6 +1,8 @@
 import { TryThisAction } from '@/store/actions/actionDecorators'
 import { resolvedPromise, rejectedPromise } from '@tests/helpers/promiseHelpers'
+import { Action } from '@/store/actions/types'
 import { Mutation } from '@/store/mutations/types'
+import { ApplicationMessage } from '@/store/interfaces/applicationMessage'
 
 describe('actionDecorators.ts', () => {
   it('calls the corresponding action and changes the application loading state correctly', async () => {
@@ -12,6 +14,19 @@ describe('actionDecorators.ts', () => {
     expect(givenContext.commit).toHaveBeenCalledWith(Mutation.SET_APPLICATION_LOADING, true)
     await returnedPromise
     expect(givenContext.commit).toHaveBeenCalledWith(Mutation.SET_APPLICATION_LOADING, false)
+  })
+
+  it('calls the corresponding action and shows an error message when it fails', async () => {
+    const givenContext = getMockedContext()
+    const givenAction = (givenContext) => rejectedPromise()
+
+    const returnedPromise = TryThisAction(givenAction)(givenContext)
+
+    expect(givenContext.commit).toHaveBeenCalledWith(Mutation.SET_APPLICATION_LOADING, true)
+    await returnedPromise
+    expect(givenContext.commit).toHaveBeenCalledWith(Mutation.SET_APPLICATION_LOADING, false)
+    const expectedErrorMessage: ApplicationMessage = { type: 'error', message: 'ha ocurrido un error' }
+    expect(givenContext.dispatch).toHaveBeenCalledWith(Action.SHOW_MESSAGE, expectedErrorMessage)
   })
 
   function getMockedContext (): any {
