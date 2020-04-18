5a0d6feabd62bf12db097d1bd3cc9b0ed0250110
Gabriel Nunez <gnunez@aidacanarias.com>
2020-04-18 18:39:16 +0100

    fixing build problems

--- vehiclesdealer-frontend/src/store/actions/actionDecorators.ts
+++ vehiclesdealer-frontend/src/store/actions/actionDecorators.ts
@@ -1,8 +1,8 @@
-import { Action } from './types'
 import { Mutation } from '@/store/mutations/types'
 import { ApplicationMessage } from '@/store/interfaces/applicationMessage'
+import { Action } from './types'
 
-export function TryThisAction (targetAction: Function): Function {
+export function TryThisAction (targetAction: Function) {
   return async function ({ commit, dispatch }: any): Promise<any> {
     commit(Mutation.SET_APPLICATION_LOADING, true)
     try {
